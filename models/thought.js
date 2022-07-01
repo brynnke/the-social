98d514ef3dea17cdf9923fdc6af40da74f52abe3
brynnke <100329799+brynnke@users.noreply.github.com>
2022-07-01 16:51:28 -0500

    working on the routes

--- models/thought.js
+++ models/thought.js
@@ -58,3 +58,11 @@ const ToughtSchema = new Schema({
         id: false
     }
 );
+
+ThoughtSchema.virtual('reactionCount').get(function (){
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
